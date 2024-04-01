842bd2b6518ea49f107cfeb64bb4d0af5a3fe3d6
Vladimirskiy Aleksandr <alexvl89@yandex.ru>
2024-04-01 10:19:46 +0300

    Init

---
+++ EasiestScheduling/MyRepeatableTask.cs
@@ -0,0 +1,27 @@
+﻿using Coravel.Invocable;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasiestScheduling;
+
+internal class MyRepeatableTask : IInvocable
+{
+    private readonly ILogger<MyRepeatableTask> _logger;
+
+    private readonly string _connectionString;
+
+    public MyRepeatableTask(ILogger<MyRepeatableTask> logger, string connectionString)
+    {
+        _logger = logger;
+        _connectionString = connectionString;
+    }
+
+    public async Task Invoke()
+    {
+        _logger.LogInformation("Hello from invocable, with {String}", _connectionString);
+    }
+}
---
+++ EasiestScheduling/Program.cs
@@ -0,0 +1,35 @@
+﻿using Coravel;
+using EasiestScheduling;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services.AddScheduler();
+builder.Services.AddTransient<MyRepeatableTask>();
+
+var app = builder.Build();
+
+app.Services.UseScheduler(async scheduler =>
+{
+    //scheduler.Schedule(() => Console.WriteLine("This was scheduled"))
+    //    .EverySeconds(2);
+
+    scheduler.ScheduleAsync(async () =>
+    {
+        await Task.Delay(20);
+
+        await Console.Out.WriteLineAsync("This is scheduled");
+
+    }).EverySeconds(2);
+
+    //scheduler.Schedule<MyRepeatableTask>()
+    //    .EverySeconds(5);
+
+    scheduler.ScheduleWithParams<MyRepeatableTask>("localhost:5001")
+        .EverySeconds(5);
+
+});
+
+
+app.Run();
